468f0274a6345d2dd01441148b9ab6eff3964f83
rafael_polonio <rafael.polonio.ext@neon.com.br>
2020-06-12 20:09:39 -0300

    developing navigation

--- app/App.js
+++ app/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { useFonts } from '@use-expo/font';
 import { StatusBar } from 'react-native';
 import { AppLoading } from 'expo';
-import Header from './src/components/Header';
-import Feed from './src/screens/Feed';
+import Routes from './src/Navigator';
 
 export default props => {
   let [fontsLoaded] = useFonts({
@@ -14,9 +13,9 @@ export default props => {
     return <AppLoading />;
   } 
   return (
-    <>
-      <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
-      <Feed />
-    </>
+      <>
+        <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
+        <Routes />
+      </>
   );
 };
---
+++ app/src/Navigator.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Feed from './screens/Feed';
+
+
+const Tab = createBottomTabNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{ gestureEnabled: false }}
+      >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          title: 'Feed',
+          tabBarIcon: ({ tintColor }) =>
+            <Icon name='home' size={30} color={tintColor} />
+        }}
+      />
+      <Tab.Screen
+        name="AddPhoto"
+        component={Feed}
+        options={{
+          title: 'Add Picture',
+          tabBarIcon: ({ tintColor }) =>
+          <Icon name='camera' size={30} color={tintColor} />
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Feed}
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ tintColor }) =>
+          <Icon name='user' size={30} color={tintColor} />
+        }}
+      />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default Routes;
